49d26f873f52d8b25a9bc12a793e4ad9a6623a89
Maxime Camp <maxime@Mini.local>
2025-11-25 20:44:34 +0100

    Deploy static site

--- _next/static/chunks/app/blog/page-434b9f40450890e0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[831],{5283:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8001,23)),Promise.resolve().then(n.t.bind(n,6382,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[1,382,721,59,358],()=>s(5283)),_N_E=e.O()}]);
